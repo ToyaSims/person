972c1cdc21ecbee5b1db8883fdc811b8235d7303
latifa <simbinelatifa@gmail.com>
2019-11-26 16:34:11 +0200

    updated code

--- person.java
+++ person.java
@@ -4,20 +4,20 @@ public class person {
     private String name,gender;
     private int age;
     private String[] interests;
-    String interest;
+
 
     public person(String name, String gender, int age, String[] interests) {
         this.name = name;
         this.gender = gender;
         this.age = age;
         this.interests = interests;
-        this.interest = "";
+
     }
 
     public String hello() {
-       // String joinedString = String.join(" ,", this.interests);
-        String str = Arrays.toString(this.interests);
-        str = str.substring(1, str.length()-1).replace(",", ",");
+       String str = String.join(" ,", this.interests);
+
+        str = str.substring(0,str.lastIndexOf( ',' )) + " and " + str.substring(str.lastIndexOf(",") + 1);
 
         return "hello,my name is " + this.name + " and I am " + this.age + " years old.my interests are " + str;
     }
